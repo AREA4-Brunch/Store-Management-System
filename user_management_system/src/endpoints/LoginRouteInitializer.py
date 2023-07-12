c047c530517464b6420b7005b127b93185387732
Sale <sale.radenkovic@gmail.com>
2023-07-12 02:56:10 +0200

    Implemented login feature.

---
+++ user_management_system/src/endpoints/LoginRouteInitializer.py
@@ -0,0 +1,122 @@
+from flask import request as flask_request
+from flask import jsonify
+from flask_jwt_extended import create_access_token, create_refresh_token
+
+from ..models import User
+from .RouteInitializer import RouteInitializer
+from .utils import is_valid_email_format
+
+
+class LoginRouteInitializer(RouteInitializer):
+    def __call__(self, app) -> None:
+        """ Init all the user management system endpoints for given app.
+        """
+        logger = app.app.logger
+        # db = app.databases["users"]["database"]
+
+        # ==================================================
+        # Endpoints:
+
+        @app.app.route('/login', methods=["POST"])
+        def login():
+            return login_user_view()
+
+        # ==================================================
+        # Helpers:
+
+        def login_user_view():
+            class UserDoesNotExist(Exception):
+                pass
+
+            class ParsingError(Exception):
+                pass
+
+            class ValidationError(Exception):
+                pass
+
+            def parse_registration_form():
+                data = dict({
+                    "email": flask_request.form.get("email", ''),
+                    "password": flask_request.form.get("password", ''),
+                })
+
+                # check if all required fields have been filled out
+                def check_missing_fields():
+                    for field_name, field_val in data.items():
+                        if field_val is None or len(field_val) == 0:
+                            raise ParsingError(f'Field {field_name} is missing.')
+
+                check_missing_fields()
+                # TODO: check for too long fields, > 256 chars
+                return data
+
+            def validate_registration_form(data):
+                def validate_email():
+                    if len(data["email"]) > 256 \
+                    or not is_valid_email_format(data["email"]):
+                        raise ValidationError('Invalid email.')
+
+                def validate_password():
+                    if not 8 <= len(data["password"]) <= 256:
+                        raise ValidationError('Invalid password.')
+
+                validate_email()
+                # validate_password()
+
+            def generate_jwts(user):
+                claims = {
+                    # 'email': user.email,  # identity
+                    'forename': user.forename,
+                    'surname': user.surname,
+                    # "roles": [ role.name for role in user.roles ]
+                }
+
+                access_token = create_access_token(
+                    identity=user.email,
+                    additional_claims=claims
+                )
+                
+                refresh_token = create_refresh_token(
+                    identity=user.email,
+                    additional_claims=claims
+                )
+
+                return access_token, refresh_token, claims
+
+            try:
+                data = parse_registration_form()
+                validate_registration_form(data)
+
+                # authenticate the user
+                user = User.query.filter_by(
+                    email=data["email"],
+                    password=data["password"],
+                ).first()
+
+                if user is None:
+                    raise UserDoesNotExist('Invalid credentials.')
+
+                access_token, refresh_token, claims = generate_jwts(user)
+
+                return jsonify({
+                    "accessToken": access_token
+                }), 200
+
+            except ParsingError as e:
+                return jsonify({
+                    "message": f'{e}'
+                }), 400
+
+            except ValidationError as e:
+                return jsonify({
+                    "message": f'{e}'
+                }), 400
+
+            except UserDoesNotExist as e:
+                return jsonify({
+                    "message": f'{e}'
+                }), 400
+
+            except Exception as e:  # unexpected error
+                logger.exception(f'Failed to login')
+                return f'Internal error: {e}', 500
--- user_management_system/src/urls.py
+++ user_management_system/src/urls.py
@@ -1,6 +1,8 @@
 from .endpoints.RegistrationRouteInitializer import RegistrationRouteInitializer
+from .endpoints.LoginRouteInitializer import LoginRouteInitializer
 
 
 ROUTE_INITIALIZERS = [
     RegistrationRouteInitializer(),
+    LoginRouteInitializer()
 ]
